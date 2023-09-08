b37fafe6a720bff3b91b392ed62b072ddf68b59c
PetoMPP <piotreq22@gmail.com>
2023-09-08 11:31:48 +0200

    feat: move flags svg to separate files (issues on Firefox with previous approach)

--- front/src/components/atoms/flag.rs
+++ front/src/components/atoms/flag.rs
@@ -57,37 +57,9 @@ impl TryFrom<String> for Country {
 
 #[function_component(Flag)]
 pub fn flag(props: &FlagProps) -> Html {
-    let svg = match &props.country {
-        Country::UnitedKingdom => {
-            html! {
-                <svg viewBox="0 0 60 30">
-                    <clipPath id="a">
-                        <path d="M0 0v30h60V0z"/>
-                    </clipPath>
-                    <clipPath id="b">
-                        <path d="M30 15h30v15zv15H0zH0V0zV0h30z"/>
-                    </clipPath>
-                    <g clip-path="url(#a)">
-                        <path d="M0 0v30h60V0z" fill="#012169"/>
-                        <path d="M0 0l60 30m0-30L0 30" stroke="#fff" stroke-width="6"/>
-                        <path d="M0 0l60 30m0-30L0 30" clip-path="url(#b)" stroke="#C8102E" stroke-width="4"/>
-                        <path d="M30 0v30M0 15h60" stroke="#fff" stroke-width="10"/><path d="M30 0v30M0 15h60" stroke="#C8102E" stroke-width="6"/>
-                    </g>
-                </svg>
-            }
-        }
-        Country::Poland => {
-            html! {
-                <svg viewBox="0 0 16 10">
-                    <path fill="#fff" d="M0 0h16v10H0z"/>
-                    <path fill="#dc143c" d="M0 5h16v5H0z"/>
-                </svg>
-            }
-        }
-    };
     html! {
         <div class={"flex items-center w-12 p-[2px]"}>
-            {svg}
+            <img src={format!("/img/flags/{}.svg", props.country.key())} />
         </div>
     }
 }
