16f56b1c20379ecd1ba715d954393dccbb59a114
PetoMPP <piotreq22@gmail.com>
2023-10-15 12:52:59 +0200

    chore: extract Editable component from PageBase

--- src/pages/page_base.rs
+++ src/pages/page_base.rs
@@ -1,17 +1,5 @@
-use crate::{
-    api::client::Client,
-    components::atoms::markdown::Markdown,
-    data::{
-        locales::{LocalesStore, TK},
-        resources::{Key, ResourceStore},
-        session::SessionStore,
-    },
-    models::user::Role,
-    router::Route,
-};
-use yew::{platform::spawn_local, prelude::*};
-use yew_router::prelude::*;
-use yewdux::prelude::*;
+use crate::components::atoms::markdown::{Editable, EditableProps};
+use yew::prelude::*;
 
 #[derive(PartialEq, Properties)]
 pub struct PageBaseProps {
@@ -30,51 +18,11 @@ pub fn page_base(props: &PageBaseProps) -> Html {
     }
 }
 
-#[derive(PartialEq, Properties)]
-pub struct EditablePageBaseProps {
-    pub reskey: String,
-}
-
 #[function_component(EditablePage)]
-pub fn editable_page_base(props: &EditablePageBaseProps) -> Html {
-    let (locales_store, _) = use_store::<LocalesStore>();
-    let (session_store, _) = use_store::<SessionStore>();
-    let (res_store, res_dispatch) = use_store::<ResourceStore>();
-    let reskey = Key {
-        reskey: props.reskey.clone(),
-        lang: locales_store.curr.key().to_string(),
-    };
-    let navigator = use_navigator().unwrap();
-    let edit_onclick = {
-        let reskey = reskey.clone();
-        Callback::from(move |_| {
-            navigator.push(&Route::Editor {
-                key: reskey.reskey.clone(),
-                lang: reskey.lang.clone(),
-            });
-        })
-    };
-    let edit_class = match &session_store.user {
-        Some(u) if u.role == Role::Admin => {
-            "btn absolute top-5 right-5 btn-accent btn-xs btn-outline"
-        }
-        _ => "hidden",
-    };
-    let markdown = res_store.get_state(&reskey).cloned().unwrap_or_default();
-    spawn_local(async move {
-        if let Ok(md) = Client::get_resource(reskey.reskey.as_str(), reskey.lang.as_str()).await {
-            if res_store.get_state(&reskey) != Some(&md) {
-                res_dispatch.reduce_mut(|store| {
-                    store.add_or_update_state(&reskey, md);
-                });
-            }
-        }
-    });
-
+pub fn editable_page_base(props: &EditableProps) -> Html {
     html! {
         <PageBase>
-            <button class={edit_class} onclick={edit_onclick}>{locales_store.get(TK::Edit)}</button>
-            <Markdown {markdown} interactive={Some(())} allowhtml={true}/>
+            <Editable reskey={props.reskey.clone()}/>
         </PageBase>
     }
 }
