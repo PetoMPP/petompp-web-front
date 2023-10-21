14e884103665b60e73b6e89c3b08dd6a7b4180f3
PetoMPP <piotreq22@gmail.com>
2023-10-21 15:51:51 +0200

    feat: create blog and blog posts base impl

--- src/pages/blog/blog.rs
+++ src/pages/blog/blog.rs
@@ -1,5 +1,5 @@
 use crate::api::client::ApiClient;
-use crate::use_effect_deps;
+use crate::data::locales::store::LocalesStore;
 use crate::{
     components::{atoms::markdown::Editable, organisms::blog::blog_summary::BlogSummary},
     pages::page_base::PageBase,
@@ -8,39 +8,69 @@ use petompp_web_models::models::tag::Tags;
 use yew::platform::spawn_local;
 use yew::prelude::*;
 use yew_router::prelude::*;
+use yewdux::prelude::*;
 
 #[function_component(Blog)]
 pub fn blog() -> Html {
     let location = use_location().unwrap();
-    let tags = location.query::<Tags>();
-    let posts = use_state(|| vec![]);
-    use_effect_deps!(|posts| {
-        spawn_local(async move {
-            let ps = ApiClient::get_posts_meta().await.unwrap();
-            posts.set(ps);
-        })
-    });
-    let posts = (*posts).clone();
-    let posts = match tags {
-        Ok(tags) => posts
-            .into_iter()
-            .filter(|meta| tags.tags().iter().any(|t| meta.tags.tags().contains(&t)))
-            .collect::<Vec<_>>(),
-        Err(_) => posts.into_iter().collect::<Vec<_>>(),
-    };
-    let posts = posts
+    let tags = location.query::<Tags>().unwrap_or_default();
+    let (locales_store, _) = use_store::<LocalesStore>();
+    let posts_int = use_mut_ref(|| None);
+    let posts = use_state(|| None);
+    use_effect_with_deps(
+        move |(posts_int, tags, curr, posts)| {
+            let posts_int = posts_int.clone();
+            let tags = tags.clone();
+            let curr = curr.clone();
+            let posts = posts.clone();
+            spawn_local(async move {
+                let posts_int = posts_int.clone();
+                if *posts_int.borrow() == None {
+                    *posts_int.borrow_mut() = Some(
+                        ApiClient::get_posts_meta()
+                            .await
+                            .unwrap()
+                            .into_iter()
+                            .collect::<Vec<_>>(),
+                    );
+                }
+                posts.set(Some(
+                    posts_int
+                        .borrow()
+                        .clone()
+                        .unwrap_or_default()
+                        .into_iter()
+                        .filter(|meta| {
+                            tags.is_empty()
+                                || tags.tags().iter().any(|t| meta.tags.tags().contains(&t))
+                        })
+                        .filter(|meta| meta.lang == curr)
+                        .collect::<Vec<_>>(),
+                ));
+            });
+        },
+        (
+            posts_int.clone(),
+            tags.clone(),
+            locales_store.curr.clone(),
+            posts.clone(),
+        ),
+    );
+    let posts = (*posts)
+        .clone()
+        .unwrap_or_default()
         .into_iter()
         .map(|meta| {
             html! {
                 <BlogSummary {meta}/>
             }
-        })
-        .collect::<Html>();
+        });
+    gloo::console::log!("rendering blog", posts.len());
     html! {
         <PageBase>
         <Editable reskey={"blog-intro".to_string()}/>
         <div class={"flex flex-col gap-2"}>
-            {posts}
+            {for posts}
         </div>
         </PageBase>
     }
