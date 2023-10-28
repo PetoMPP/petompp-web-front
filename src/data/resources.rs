4491709ef1eccabfea9ec0698ee423fd77bcfc76
PetoMPP <piotreq22@gmail.com>
2023-10-28 15:46:19 +0200

    feat: create resource/post selection in editor

--- src/data/resources.rs
+++ src/data/resources.rs
@@ -10,40 +10,40 @@ pub struct ResourceId {
     blob: Option<String>,
 }
 
-impl TryInto<ResId> for ResourceId {
+impl TryInto<(ResId, Country)> for ResourceId {
     type Error = String;
 
-    fn try_into(self) -> Result<ResId, Self::Error> {
+    fn try_into(self) -> Result<(ResId, Country), Self::Error> {
         match (self.key, self.blob) {
             (Some(key), None) => {
                 let (path, lang) = key.split_once("@").ok_or("invalid key")?;
-                Ok(ResId::ResKey((
-                    path.to_string(),
+                Ok((
+                    ResId::ResKey(path.to_string()),
                     Country::try_from(lang).map_err(|_| "invalid lang")?,
-                )))
+                ))
             }
             (None, Some(blob)) => {
                 let (path, lang) = blob.split_once("@").ok_or("invalid blob")?;
-                Ok(ResId::Blob((
-                    path.to_string(),
+                Ok((
+                    ResId::Blob(path.to_string()),
                     Country::try_from(lang).map_err(|_| "invalid lang")?,
-                )))
+                ))
             }
             _ => Err("invalid resource id".to_string()),
         }
     }
 }
 
-impl From<ResId> for ResourceId {
-    fn from(value: ResId) -> Self {
-        match value {
-            ResId::ResKey((p, l)) => Self {
-                key: Some(format!("{}@{}", p, l.key())),
+impl From<(ResId, Country)> for ResourceId {
+    fn from(value: (ResId, Country)) -> Self {
+        match value.0 {
+            ResId::ResKey(p) => Self {
+                key: Some(format!("{}@{}", p, value.1.key())),
                 blob: None,
             },
-            ResId::Blob((p, l)) => Self {
+            ResId::Blob(p) => Self {
                 key: None,
-                blob: Some(format!("{}@{}", p, l.key())),
+                blob: Some(format!("{}@{}", p, value.1.key())),
             },
         }
     }
@@ -51,30 +51,28 @@ impl From<ResId> for ResourceId {
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum ResId {
-    ResKey((String, Country)),
-    Blob((String, Country)),
+    ResKey(String),
+    Blob(String),
 }
 
 impl ResId {
-    pub fn with_lang(self, lang: Country) -> Self {
+    pub fn id(&self) -> &str {
         match self {
-            Self::ResKey((p, _)) => Self::ResKey((p, lang)),
-            Self::Blob((p, _)) => Self::Blob((p, lang)),
+            Self::ResKey(id) | Self::Blob(id) => id,
         }
     }
 
-    pub fn lang(&self) -> &Country {
+    pub fn type_str(&self) -> &'static str {
         match self {
-            Self::ResKey((_, l)) | Self::Blob((_, l)) => l,
+            Self::ResKey(_) => "resource",
+            Self::Blob(_) => "blog post",
         }
     }
 
-    pub async fn get_value(&self) -> Result<String, RequestError> {
+    pub async fn get_value(&self, lang: &Country) -> Result<String, RequestError> {
         match self {
-            Self::ResKey((reskey, lang)) => {
-                ApiClient::get_resource(reskey.as_str(), lang.key()).await
-            }
-            Self::Blob((path, lang)) => {
+            Self::ResKey(reskey) => ApiClient::get_resource(reskey.as_str(), lang).await,
+            Self::Blob(path) => {
                 BlobClient::get_post_content(format!("{}/{}.md", path, lang.key()).as_str()).await
             }
         }
