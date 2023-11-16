030b3da5f84fe915f0a5ef83ab19fd94ebfb62fb
PetoMPP <piotreq22@gmail.com>
2023-11-16 18:42:37 +0100

    feat: cache for metadata

--- src/components/organisms/blog/blog_meta_editor.rs
+++ src/components/organisms/blog/blog_meta_editor.rs
@@ -1,3 +1,4 @@
+use self::macros::onchange;
 use crate::{
     components::atoms::{
         label::Label,
@@ -20,22 +21,41 @@ pub struct BlogMetaEditorProps {
 
 #[function_component(BlogMetaEditor)]
 pub fn blog_meta_editor(props: &BlogMetaEditorProps) -> Html {
-    let title_onchange = {
-        let props = props.clone();
-        props.ondatachanged.clone().reform(move |title: String| {
-            let mut data = props.data.clone().unwrap_or_default();
-            data.title = title;
-            data
-        })
-    };
+    let title_onchange = onchange!(props, props.data.title);
+    let tags_onchange = onchange!(props, props.data.tags);
+    let image_onchange = onchange!(props, props.data.image);
+    let summary_onchange = onchange!(props, props.data.summary);
+
     html! {
         <>
-            <TextInput label={"Title"} itype={InputType::Text} enabled={true} value={props.data.as_ref().map(|d| d.title.clone()).unwrap_or_default()} onchange={title_onchange}/>
-            <BlogTagsEditor data={props.data.as_ref().map(|d| d.tags.clone())} ondatachanged={Callback::noop()}/>
-            <TextInput label={"Created"} itype={InputType::Text} enabled={false} value={props.data.as_ref().map(|d| d.created.format("%Y-%m-%d %H:%M:%S").to_string()).unwrap_or_default()}/>
-            <TextInput label={"Updated"} itype={InputType::Text} enabled={false} value={props.data.as_ref().map(|d| d.updated.format("%Y-%m-%d %H:%M:%S").to_string()).unwrap_or_default()}/>
-            <TextInput label={"Image"} itype={InputType::Text} enabled={true} value={props.data.as_ref().map(|d| d.image.clone()).unwrap_or_default()}/>
-            <TextareaInput label={"Summary"} enabled={true} value={props.data.as_ref().map(|d| d.summary.clone()).unwrap_or_default()}/>
+            <TextInput
+                label={"Title"}
+                itype={InputType::Text}
+                enabled={true}
+                value={props.data.as_ref().map(|d| d.title.clone()).unwrap_or_default()}
+                onchange={title_onchange}/>
+            <BlogTagsEditor data={props.data.as_ref().map(|d| d.tags.clone())} ondatachanged={tags_onchange}/>
+            <TextInput
+                label={"Created"}
+                itype={InputType::Text}
+                enabled={false}
+                value={props.data.as_ref().map(|d| d.created.format("%Y-%m-%d %H:%M:%S").to_string()).unwrap_or_default()}/>
+            <TextInput
+                label={"Updated"}
+                itype={InputType::Text}
+                enabled={false}
+                value={props.data.as_ref().map(|d| d.updated.format("%Y-%m-%d %H:%M:%S").to_string()).unwrap_or_default()}/>
+            <TextInput
+                label={"Image"}
+                itype={InputType::Text}
+                enabled={true}
+                value={props.data.as_ref().map(|d| d.image.clone()).unwrap_or_default()}
+                onchange={image_onchange}/>
+            <TextareaInput
+                label={"Summary"}
+                enabled={true}
+                value={props.data.as_ref().map(|d| d.summary.clone()).unwrap_or_default()}
+                onchange={summary_onchange}/>
         </>
     }
 }
@@ -51,36 +71,44 @@ pub fn blog_tags_editor(props: &BlogTagsEditorProps) -> Html {
     let tags = use_state_eq(|| props.data.clone().unwrap_or_default().tags());
     let tag_nodes = {
         let tags = tags.clone();
+        let ondatachanged = props.ondatachanged.clone();
         (*tags).clone().into_iter()
             .map(move |t| {
                 let onclick = {
                     let t = t.clone();
                     let tags = tags.clone();
+                    let ondatachanged = ondatachanged.clone();
                     Callback::from(move |_| {
-                        tags.set(tags.iter().filter(|x| x != &&t).cloned().collect::<Vec<Tag>>());
+                        let new_tags = tags.iter().filter(|x| x != &&t).cloned().collect::<Vec<Tag>>();
+                        tags.set(new_tags.clone());
+                        ondatachanged.emit(new_tags.into());
                 })};
                 html! { <span {onclick} class={"flex btn btn-xs normal-case rounded-full"}>{&*t}<div class={"w-4 h-4 bg-base-content"} style={get_svg_bg_mask_style("/img/ui/x.svg")} /></span> }})
             .collect::<VNode>()
     };
-    let onkeydown = Callback::from(move |e: KeyboardEvent| {
-        if e.key() != "Enter" {
-            return;
-        }
-        let element: HtmlInputElement = e.target_unchecked_into();
-        let tag = Tag {
-            tag: element.value(),
-        };
-        if tag.is_empty() || tags.contains(&tag) {
-            return;
-        }
-        element.set_value("");
-        tags.set(
-            tags.iter()
+    let onkeydown = {
+        let ondatachanged = props.ondatachanged.clone();
+        Callback::from(move |e: KeyboardEvent| {
+            if e.key() != "Enter" {
+                return;
+            }
+            let element: HtmlInputElement = e.target_unchecked_into();
+            let tag = Tag {
+                tag: element.value(),
+            };
+            if tag.is_empty() || tags.contains(&tag) {
+                return;
+            }
+            element.set_value("");
+            let new_tags = tags
+                .iter()
                 .cloned()
                 .chain(std::iter::once(tag))
-                .collect::<Vec<Tag>>(),
-        );
-    });
+                .collect::<Vec<Tag>>();
+            tags.set(new_tags.clone());
+            ondatachanged.emit(new_tags.into());
+        })
+    };
     html! {
         <Label label={"Tags"}>
             <div class={"flex h-full gap-2 py-3 items-center input input-bordered shadow-md flex-wrap"}>
@@ -89,4 +117,21 @@ pub fn blog_tags_editor(props: &BlogTagsEditorProps) -> Html {
             </div>
         </Label>
     }
+}
+
+mod macros {
+    macro_rules! onchange {
+        ($props:expr, $_0:ident.$_1:ident.$field:ident) => {{
+            let props = $props.clone();
+            props
+                .ondatachanged
+                .clone()
+                .reform(move |data| BlogMetaData {
+                    $field: data,
+                    ..props.data.clone().unwrap_or_default()
+                })
+        }};
+    }
+
+    pub(crate) use onchange;
 }
