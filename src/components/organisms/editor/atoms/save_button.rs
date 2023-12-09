9cd1981b35f8a422ddf59c807f7a753647068563
PetoMPP <piotreq22@gmail.com>
2023-12-09 14:29:56 +0100

    #20: Add modals to editor with localization

--- src/components/organisms/editor/atoms/save_button.rs
+++ src/components/organisms/editor/atoms/save_button.rs
@@ -1,29 +1,24 @@
 use crate::{
     api::client::ApiClient,
+    async_event,
+    components::atoms::modal::{show_modal_callback, Buttons, ModalButton, ModalData, ModalStore},
     data::{
         locales::{store::LocalesStore, tk::TK},
         resources::{id::ResId, store::LocalStore},
         session::SessionStore,
     },
-    pages::editor::EditorState,
+    pages::editor::{EditorProps, EditorState},
 };
-use petompp_web_models::models::{blog_data::BlogData, country::Country};
-use yew::{platform::spawn_local, prelude::*};
+use petompp_web_models::models::blog_data::BlogData;
+use yew::prelude::*;
 use yewdux::prelude::*;
 
-#[derive(Clone, PartialEq, Properties)]
-pub struct Props {
-    pub state: EditorState,
-    pub onstatechanged: Callback<EditorState>,
-    pub resid: Option<ResId>,
-    pub lang: Option<Country>,
-}
-
 #[function_component(SaveButton)]
-pub fn save_button(props: &Props) -> Html {
+pub fn save_button(props: &EditorProps) -> Html {
     let (session_store, _) = use_store::<SessionStore>();
     let (local_store, local_dispatch) = use_store::<LocalStore>();
     let (locales_store, _) = use_store::<LocalesStore>();
+    let (_, modal_dispatch) = use_store::<ModalStore>();
     let (Some(resid), Some(lang)) = (&props.resid, &props.lang) else {
         return html! {};
     };
@@ -33,47 +28,19 @@ pub fn save_button(props: &Props) -> Html {
     if let EditorState::Loading = &props.state {
         return html! {};
     }
-    let onstatechange = props.onstatechanged.clone();
-    let local_dispatch = local_dispatch.clone();
-    let resid = resid.clone();
-    let lang = *lang;
-    let value = value.clone();
-    let meta = meta.clone();
+    let onstatechange = &props.onstatechanged;
     let token = session_store.token.clone().unwrap_or_default();
     let isnew = matches!(&props.state, EditorState::Ok(Some((Some(true), _, _))));
-    let onclick = Callback::from(move |_| {
-        let onstatechange = onstatechange.clone();
-        let local_dispatch = local_dispatch.clone();
-        let resid = resid.clone();
-        let lang = lang;
-        let value = value.clone();
-        let meta = meta.clone();
-        let token = token.clone();
-        spawn_local(async move {
+    let onclick = async_event!(
+        |onstatechange, local_dispatch, resid, lang, value, meta, token| {
             onstatechange.emit(EditorState::Loading);
-            match &resid {
+            match match &resid {
                 ResId::ResKey(key) => match isnew {
-                    true => match ApiClient::create_resource(&token, key, &lang, &value).await {
-                        Ok(_) => {
-                            local_dispatch.reduce_mut(|store| store.remove(&resid, lang.key()));
-                            onstatechange.emit(EditorState::Ok(None));
-                        }
-                        Err(e) => {
-                            onstatechange.emit(EditorState::Err(e));
-                        }
-                    },
-                    false => match ApiClient::update_resource(&token, key, &lang, &value).await {
-                        Ok(_) => {
-                            local_dispatch.reduce_mut(|store| store.remove(&resid, lang.key()));
-                            onstatechange.emit(EditorState::Ok(None));
-                        }
-                        Err(e) => {
-                            onstatechange.emit(EditorState::Err(e));
-                        }
-                    },
+                    true => ApiClient::create_resource(&token, key, &lang, &value).await,
+                    false => ApiClient::update_resource(&token, key, &lang, &value).await,
                 },
                 ResId::Blob(_) => {
-                    match ApiClient::create_or_update_post(
+                    ApiClient::create_or_update_post(
                         resid.id(),
                         lang.key(),
                         &token,
@@ -83,22 +50,45 @@ pub fn save_button(props: &Props) -> Html {
                         },
                     )
                     .await
-                    {
-                        Ok(_) => {
-                            local_dispatch.reduce_mut(|store| store.remove(&resid, lang.key()));
-                            onstatechange.emit(EditorState::Ok(None));
-                        }
-                        Err(e) => {
-                            onstatechange.emit(EditorState::Err(e));
-                        }
-                    }
+                }
+            } {
+                Ok(_) => {
+                    local_dispatch.reduce_mut(|store| store.remove(&resid, lang.key()));
+                    onstatechange.emit(EditorState::Ok(None));
+                }
+                Err(e) => {
+                    onstatechange.emit(EditorState::Err(e));
                 }
             }
-        });
-    });
+        }
+    );
+    let (text, title, message) = match isnew {
+        true => (
+            locales_store.get(TK::Create),
+            locales_store.get(TK::CreateResource),
+            locales_store.get(TK::CreateResourceQuestion),
+        ),
+        false => (
+            locales_store.get(TK::Save),
+            locales_store.get(TK::SaveChanges),
+            locales_store.get(TK::SaveChangesQuestion),
+        ),
+    };
+    let onclick = show_modal_callback(
+        ModalData {
+            title,
+            message,
+            buttons: Buttons::ConfirmCancel(
+                ModalButton::new(locales_store.get(TK::Save), Some(onclick)),
+                ModalButton::new(locales_store.get(TK::Cancel), None),
+            ),
+        },
+        modal_dispatch.clone(),
+    );
+
     html! {
         <button class={"btn btn-success grow"} {onclick}>
-        {locales_store.get(TK::Save)}
+            {text}
         </button>
     }
 }
