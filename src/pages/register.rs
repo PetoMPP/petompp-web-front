0e65d2e2f844ff472abf980a714a572a11a113d6
PetoMPP <piotreq22@gmail.com>
2023-12-28 18:31:30 +0100

    fix #42: Add registration validation on front end, rework how errors are displayed

--- src/pages/register.rs
+++ src/pages/register.rs
@@ -1,5 +1,6 @@
 use crate::api::client::ApiClient;
 use crate::components::atoms::text_input::{InputType, TextInput};
+use crate::components::state::State;
 use crate::data::locales::localizable::Localizable;
 use crate::data::locales::store::LocalesStore;
 use crate::data::locales::tk::TK;
@@ -8,73 +9,78 @@ use crate::{
     api::client::RequestError, async_event, components::atoms::modal::show_error,
     pages::page_base::PageBase,
 };
-use petompp_web_models::error::{Error, UsernameValidationError, ValidationError};
+use petompp_web_models::error::Error;
 use petompp_web_models::models::credentials::Credentials;
-use std::fmt::Display;
+use yew::platform::spawn_local;
 use yew::prelude::*;
 use yew_router::prelude::*;
 use yewdux::prelude::*;
 
-#[derive(Clone, Debug, PartialEq)]
-enum RegisterError {
-    Username(String),
-    Password(String),
-}
-
-impl Display for RegisterError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            RegisterError::Username(e) => write!(f, "{}", e),
-            RegisterError::Password(e) => write!(f, "{}", e),
-        }
-    }
-}
-
 #[function_component(Register)]
 pub fn register() -> Html {
-    let form_data = use_mut_ref(Credentials::default);
-    let error_state = use_state_eq(|| Option::None);
+    let form_data = use_state(Credentials::default);
+    let requirements = use_state(|| State::Ok(None));
     let (locales_store, _) = use_store::<LocalesStore>();
     let history = use_navigator().unwrap();
+    use_effect_with_deps(
+        move |requirements| {
+            let requirements = requirements.clone();
+            match &*requirements {
+                State::Ok(Some(_)) | State::Loading | State::Err(_) => return,
+                _ => {}
+            }
+            requirements.set(State::Loading);
+            spawn_local(async move {
+                match ApiClient::get_user_settings().await {
+                    Ok(req) => requirements.set(State::Ok(Some(req))),
+                    Err(error) => requirements.set(State::Err(error)),
+                }
+            });
+        },
+        requirements.clone(),
+    );
 
     let onchange_username = {
-        let error_state = error_state.clone();
         let form_data = form_data.clone();
         Callback::from(move |e| {
-            form_data.borrow_mut().name = e;
-            error_state.set(Option::None);
+            form_data.set(Credentials {
+                name: e,
+                ..(*form_data).clone()
+            });
         })
     };
     let onchange_password = {
-        let error_state = error_state.clone();
         let form_data = form_data.clone();
         Callback::from(move |e| {
-            form_data.borrow_mut().password = e;
-            error_state.set(Option::None);
+            form_data.set(Credentials {
+                password: e,
+                ..(*form_data).clone()
+            });
         })
     };
     let onsubmit = async_event!(
-    [prevent SubmitEvent] |form_data, history, error_state, locales_store| {
-            let creds = form_data.borrow().clone();
+    [prevent SubmitEvent] |form_data, history, locales_store, requirements| {
+            let creds = (*form_data).clone();
             match ApiClient::register(creds).await {
             Ok(()) => {
-                error_state.set(Option::None);
                 history.push(&Route::Login);
             },
             Err(error) => {
                 match error {
                     RequestError::Endpoint(_, error) => {
                         match &error {
-                            Error::UserNameTaken(_) => error_state.set(Some(RegisterError::Username(error.localize(&locales_store)))),
-                            Error::ValidationError(ve) => match ve {
-                                ValidationError::Username(ue) => match ue {
-                                    UsernameValidationError::InvalidLength(_, _) |
-                                    UsernameValidationError::InvalidCharacters(_) => error_state.set(Some(RegisterError::Username(error.localize(&locales_store)))),
-                                },
-                                ValidationError::Password(_) => error_state.set(Some(RegisterError::Password(error.localize(&locales_store)))),
-                                _ => show_error(error.localize(&locales_store), Some((&Route::Home, &history))),
+                            Error::Register(reg_err) => {
+                                let message = match &*requirements {
+                                    State::Ok(Some((ur, pr))) => {
+                                        let name_error = (ur, &reg_err.username_errors.iter().map(|s|s.as_str()).collect::<Vec<&str>>()).localize(&locales_store);
+                                        let pass_error = (pr, &reg_err.password_errors.iter().map(|s|s.as_str()).collect::<Vec<&str>>()).localize(&locales_store);
+                                        name_error + "\n" + pass_error.as_str()
+                                    },
+                                    _ => reg_err.username_errors.join("\n") + "\n" + reg_err.password_errors.join("\n").as_str(),
+                                };
+                                show_error(message, None)
                             },
-                            _ => show_error(error.localize(&locales_store), Some((&Route::Home, &history))),
+                            _ => show_error(error.localize(&locales_store), None),
                         }
                     }
                     RequestError::Parse(error) | RequestError::Network(error) => {
@@ -84,13 +90,19 @@ pub fn register() -> Html {
             }
         }
     });
-    let username_error = match &*error_state {
-        Some(RegisterError::Username(error)) => Some(error.clone()),
-        _ => None,
-    };
-    let password_error = match &*error_state {
-        Some(RegisterError::Password(error)) => Some(error.clone()),
-        _ => None,
+    let (name_err, pass_err) = match &*requirements {
+        State::Ok(Some((ur, pr))) => {
+            let name_error = match (ur, &form_data.name).localize(&locales_store) {
+                s if s.is_empty() => None,
+                s => Some(s),
+            };
+            let pass_error = match (pr, &form_data.password).localize(&locales_store) {
+                s if s.is_empty() => None,
+                s => Some(s),
+            };
+            (name_error, pass_error)
+        }
+        _ => (None, None),
     };
     html! {
         <PageBase title={locales_store.get(TK::Register)}>
@@ -101,11 +113,11 @@ pub fn register() -> Html {
             <TextInput
                 label={locales_store.get(TK::Username)} itype={InputType::Text} enabled={true}
                 placeholder={locales_store.get(TK::TypeUsername)} autocomplete={"username"}
-                onchange={onchange_username} error={username_error}/>
+                onchange={onchange_username} error={name_err}/>
             <TextInput
                 label={locales_store.get(TK::Password)} itype={InputType::Password} enabled={true}
                 placeholder={locales_store.get(TK::TypePassword)} autocomplete={"new-password"}
-                onchange={onchange_password} error={password_error}/>
+                onchange={onchange_password} error={pass_err}/>
             <button class={"btn btn-primary shadow-md lg:text-xl mt-4"}>{locales_store.get(TK::Register)}</button>
         </form>
         </PageBase>
