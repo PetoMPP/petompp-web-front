9bfc25ed3afdb878389dfdce9056c88357d2ebd9
PetoMPP <piotreq22@gmail.com>
2023-12-28 18:31:30 +0100

    fix #42: Add registration validation on front end, rework how errors are displayed

--- src/data/locales/error.rs
+++ src/data/locales/error.rs
@@ -1,40 +1,28 @@
 use super::{localizable::Localizable, tk::TK};
 use petompp_web_models::error::{
-    AuthError, Error, QueryValidationError, ResourceDataValidationError, UsernameValidationError,
+    AuthError, Error, QueryValidationError, ResourceDataValidationError, UserError,
     ValidationError,
 };
 
 impl Localizable for Error {
     fn localize(&self, locales: &super::store::LocalesStore) -> String {
         match self.to_owned() {
-            Error::AuthError(e) => match e {
+            Error::Auth(e) => match e {
                 AuthError::MissingClaim(c) => locales.get(TK::E_Auth_MissingClaim(c)),
                 AuthError::InvalidFormat(c) => locales.get(TK::E_Auth_InvalidFormat(c)),
                 AuthError::TokenExpiredS(s) => locales.get(TK::E_Auth_TokenExpiredS(s as i32)),
                 AuthError::JwtError(s) => locales.get(TK::E_Auth_JwtError(s)),
             },
-            Error::DatabaseError(db) => locales.get(TK::E_Database(db)),
-            Error::DatabaseConnectionError(dbc) => locales.get(TK::E_DatabaseConnection(dbc)),
-            Error::UserNameTaken(u) => locales.get(TK::E_UserNameTaken(u)),
-            Error::UserNotFound(u) => locales.get(TK::E_UserNotFound(u)),
-            Error::InvalidCredentials => locales.get(TK::E_InvalidCredentials),
-            Error::UserNotConfirmed(u) => locales.get(TK::E_UserNotConfirmed(u)),
-            Error::ValidationError(ve) => match ve {
-                ValidationError::Username(ue) => match ue {
-                    UsernameValidationError::InvalidLength(min, max) => {
-                        locales.get(TK::E_Validation_Username_InvalidLength(min, max))
-                    }
-                    UsernameValidationError::InvalidCharacters(chars) => {
-                        locales.get(TK::E_Validation_Username_InvalidCharacters(
-                            chars
-                                .iter()
-                                .map(|c| c.to_string())
-                                .collect::<Vec<_>>()
-                                .join(", "),
-                        ))
-                    }
-                },
-                ValidationError::Password(pr) => locales.get(TK::E_Validation_Password(pr)),
+            Error::Database(db) => locales.get(TK::E_Database(db)),
+            Error::DatabaseConnection(dbc) => locales.get(TK::E_DatabaseConnection(dbc)),
+            Error::User(ue) => match ue {
+                UserError::NameTaken(u) => locales.get(TK::E_UserNameTaken(u)),
+                UserError::NotFound(u) => locales.get(TK::E_UserNotFound(u)),
+                UserError::InvalidCredentials => locales.get(TK::E_InvalidCredentials),
+                UserError::NotConfirmed(u) => locales.get(TK::E_UserNotConfirmed(u)),
+            },
+            Error::Register(_) => "use localize with Requirements!".into(),
+            Error::Validation(ve) => match ve {
                 ValidationError::Country => locales.get(TK::E_Validation_Country),
                 ValidationError::Query(qe) => match qe {
                     QueryValidationError::InvalidColumn(c) => {
