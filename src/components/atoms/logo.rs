edf4ba5884bbb3cb394dfb396cddead1e9756931
PetoMPP <piotreq22@gmail.com>
2024-02-13 16:40:53 +0100

    resolves #47: add favicon and logo

--- src/components/atoms/logo.rs
+++ src/components/atoms/logo.rs
@@ -1,12 +1,20 @@
-use crate::router::route::Route;
+use crate::{
+    hooks::color_scheme::{use_color_scheme, ColorScheme},
+    router::route::Route,
+};
 use yew::prelude::*;
 use yew_router::prelude::*;
 
 #[function_component(Logo)]
 pub fn logo() -> Html {
     let navigator = use_navigator().unwrap();
+    let color_scheme = use_color_scheme();
     let onclick = Callback::from(move |_| navigator.push(&Route::Home));
+    let src = match color_scheme {
+        ColorScheme::Light => "img/logo_light.png",
+        ColorScheme::Dark => "img/logo_dark.png",
+    };
     html! {
-        <button class={"btn btn-ghost text-xl"} {onclick}>{"PetoMPP.NET"}</button>
+        <img {src} class={"btn btn-ghost"} {onclick} />
     }
 }
